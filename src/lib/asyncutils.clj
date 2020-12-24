0a7cb0468f9ee824d520944f9f2c54e9623e8811
Mike Longworth <mail@mikelongworth.co.uk>
2020-12-24 10:09:21 +0000

    remove support for cljs.core.ExceptionInfo

--- src/lib/asyncutils.clj
+++ src/lib/asyncutils.clj
@@ -1,13 +1,5 @@
 (ns lib.asyncutils)                                         ;macros
 
-;(defmacro go [& body]
-;   (cons 'cljs.core.async.macros/go body)
-;   )
-;
-;(defmacro go-loop [bindings & body]
-;   (concat `(cljs.core.async.macros/go-loop bindings) body)
-;   )
-
 (defmacro >!x
   "The same as (do (>! c val) (close! c))"
   [c val]
@@ -25,56 +17,31 @@
   [& body]
   ;for repl use
   ;cljs.core.async/go prints caught exceptions to the console but NOT the repl!!
-  (list 'cljs.core.async/go (concat (cons 'try body) `((catch js/Error e (println e))
-                                                       (catch cljs.core.ExceptionInfo e (println e))
-                                                       )))
+  (list 'cljs.core.async/go (concat (cons 'try body) `((catch :default val# (println val#)))))
   )
 
-(defmacro go? [& body]
-  (list 'cljs.core.async/go (concat (cons 'try body) `((catch js/Error e e) (catch cljs.core.ExceptionInfo e e))))
+(defmacro go?
+  "'go' enhanced to catch and return js/Error as channel value"
+  [& body]
+  (list 'cljs.core.async/go (concat (cons 'try body) `((catch :default val# val#))))
   )
-
-#_(defmacro go? [& body]
-    `(go
-       (try
-         ~@body
-         (catch js/Error e e)
-         (catch cljs.core.ExceptionInfo e e)
-         )))
 
 (defmacro <?
   "Takes a value from a core.async channel, throwing the value if it
   is a js/Error."
   ([c e]
    `(let [val# (cljs.core.async/<! ~c)]
-      (if (or (instance? js/Error val#) (instance? cljs.core.ExceptionInfo val#))
+      (if (instance? js/Error val#)
         (when ~e (~e val#))
         val#)))
   ([c]
    `(let [val# (cljs.core.async/<! ~c)]
-      (if (or (instance? js/Error val#) (instance? cljs.core.ExceptionInfo val#))
+      (if (instance? js/Error val#)
         (throw val#)
         val#)))
   )
 
-#_(defmacro go-let
-    "Like (go (let ...))"
-    [bindings & body]
-    `(go (let ~bindings ~@body)))
-
 (defmacro go-let
   "Like (go (let ...))"
   [bindings & body]
-  (list 'go (cons 'let (cons bindings body))))
-
-#_(defn test-<? []                                          ;copy to cljs
-    (go
-      (<? (go?
-            (let [c< (chan 1)]
-              (put! c< (ex-info "error" nil))
-              (we :got (<? c< #(we :x %)))
-              (put! c< (ex-info "error" nil))
-              (we :got (<? c<))
-              :z
-              )) #(we :c %)
-          )))
+  (list 'cljs.core.async/go (cons 'let (cons bindings body))))
