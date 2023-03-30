b89afd7b86c04fa46d14929ddf1d44957938c153
Mike Longworth <mail@mikelongworth.co.uk>
2023-03-30 17:45:00 +0100

    publish app from docs folder

--- build-service-worker.js
+++ build-service-worker.js
@@ -1,10 +1,10 @@
 //node .\build-service-worker.js
 const {generateSW} = require('workbox-build');
-const swDest = 'doc/service-worker.js';
+const swDest = 'docs/service-worker.js';
 generateSW({
   //swSrc: 'src-js/service-worker.js',
   swDest,
-  globDirectory: 'doc',
+  globDirectory: 'docs',
   // Other configuration options...
 }).then(({count, size}) => {
   console.log(`Generated ${swDest}, which will precache ${count} files, totaling ${size} bytes.`);
