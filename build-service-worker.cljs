7f87af8bb5e6b1a8100b0c02b6339d58c9d16148
Mike Longworth <mail@mikelongworth.co.uk>
2024-01-29 08:35:49 +0000

    Precache externals

--- build-service-worker.cljs
+++ build-service-worker.cljs
@@ -3,22 +3,54 @@
   (:require 
    ["workbox-build" :as workbox]
    [clojure.pprint :refer [pprint]]
+   ["glob" :refer [glob]]
+   [promesa.core :as p]
+   [clojure.set :refer [difference]]
    ))
 
-(defn generate-sw []
-  (let [sw-dest "docs/service-worker.js"]
-    (-> (workbox/generateSW #js {:swDest "docs/service-worker.js"
-                                 :globDirectory "docs"
-                                 :maximumFileSizeToCacheInBytes (* 50 1024 1024)
-                               ;; Other configuration options can be added here
-                                 })
-        (.then (fn [result]
-                 (let [{:keys [count size warnings] :as r} (js->clj result :keywordize-keys true)]
-                   ;(pprint r)
-                   (println)
-                   (doseq [w warnings]
-                     (println w))
-                   (println (str "Generated " sw-dest ", which will precache " count " files, totaling " size " bytes."))
-                   ))))))
+(def target-dir "docs")
+(def cache-patterns ["**/*.{html,js,css,png,gif,ico,jpg,svg,json}"])
+(def external [{:url "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" :revision "1"}
+               {:url "https://unpkg.com/react-quill@1.3.3/dist/quill.snow.css" :revision "1.3.3"}])
 
-(generate-sw)
+(defn $glob [& args]
+  (apply glob (map clj->js args)))
+
+(defn list-files []
+  (p/let [all ($glob ["**/*.*"] {:cwd target-dir})
+          all (concat all external)
+          cache ($glob cache-patterns {:cwd target-dir 
+                                       :ignore ["service-worker.js" "workbox-*.js"] ;handled by default
+                                       })
+          cache (concat cache external)
+          not-cached (difference (-> all set) cache)]
+    (println "\n-- all:")
+    (doseq [f all]
+      (println f))
+    (println "\n-- not-cached:")
+    (doseq [f not-cached]
+      (println f))
+    (println "-- All:" (count all) " Caching:" (count cache))))
+
+(defn generate-sw []
+  (println)
+  (p/let [sw-dest "docs/service-worker.js"
+          $r (workbox/generateSW (clj->js
+                                  {:swDest sw-dest 
+                                   :globDirectory target-dir
+                                   :globPatterns cache-patterns
+                                   :maximumFileSizeToCacheInBytes (* 50 1024 1024)
+                                   :additionalManifestEntries external
+                                   :runtimeCaching [{:urlPattern #".*"
+                                                     :handler "CacheFirst"
+                                                     }]
+                                   }))
+          {:keys [count size warnings] :as r} (js->clj $r :keywordize-keys true)]
+    ;(pprint r)
+    (doseq [w warnings]
+      (println w))
+    (println (str "Generated " sw-dest ", which will pre-cache " count " files, totaling " size " bytes."))))
+
+(p/do 
+  (list-files)
+  (generate-sw))
