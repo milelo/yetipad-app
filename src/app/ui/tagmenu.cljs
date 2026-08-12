4a3852013a021b5b5216e3b21e6df98ee9990316
Mike Longworth <mail@mikelongworth.co.uk>
2026-08-12 11:01:45 +0100

    Add tagmenu tag long press context menu with open option

--- src/app/ui/tagmenu.cljs
+++ src/app/ui/tagmenu.cljs
@@ -7,13 +7,47 @@
     [app.subs :as subs]
     [app.events :as events]
     [cljs.pprint :refer [pprint]]
-    ["@mui/material" :refer [MenuList MenuItem Popover Typography]]
+    ["@mui/material" :refer [Menu MenuList MenuItem Popover Typography]]
     ["@mui/icons-material/ArrowForwardIos" :default more-icon]
     ))
 
 (def log (log/logger 'app.ui.tagmenu))
 
 (defonce selected-elements* (r/atom {}))
+(defonce context-menu* (r/atom nil))
+(defonce long-press-timer* (atom nil))
+
+(defn close-context-menu! []
+  (reset! context-menu* nil))
+
+(defn open-tag! [id]
+  (reset! selected-elements* {})
+  (close-context-menu!)
+  (events/open-tag-drawer! false)
+  (events/open-item! id {:disable-toggle true}))
+
+(defn open-context-menu! [id e]
+  (.preventDefault e)
+  (reset! context-menu* {:id       id
+                         :position {:top    (.-clientY e)
+                                    :left   (.-clientX e)}}))
+
+(defn cancel-long-press! []
+  (when-let [timer @long-press-timer*]
+    (js/clearTimeout timer)
+    (reset! long-press-timer* nil)))
+
+(defn start-long-press! [id e]
+  (cancel-long-press!)
+  (let [touch (aget (.-touches e) 0)]
+    (reset! long-press-timer*
+            (js/setTimeout
+              (fn []
+                (reset! long-press-timer* nil)
+                (reset! context-menu* {:id       id
+                                       :position {:top  (.-clientY touch)
+                                                  :left (.-clientX touch)}}))
+              500))))
 
 (defn tag-submenu [tid tag-items other-items]
   [:> MenuList
@@ -27,16 +61,19 @@
                                                            (if (= kind :tag)
                                                              (let [element (-> e.currentTarget.childNodes first)]
                                                                (swap! selected-elements* update id #(if % false element)))
-                                                             (do
-                                                               (reset! selected-elements* {})
-                                                               (events/open-tag-drawer! false)
-                                                               (events/open-item! id {:disable-toggle true})
-                                                               )))
+                                                             (open-tag! id)))
                                                :on-double-click (when (= kind :tag)
-                                                                  (fn [_]
-                                                                    (reset! selected-elements* {})
-                                                                    (events/open-tag-drawer! false)
-                                                                    (events/open-item! id {:disable-toggle true})))
+                                                                  #(open-tag! id))
+                                               :on-context-menu (when (= kind :tag)
+                                                                  #(open-context-menu! id %))
+                                               :on-touch-start (when (= kind :tag)
+                                                                 #(start-long-press! id %))
+                                               :on-touch-end (when (= kind :tag)
+                                                               (fn [_]
+                                                                 (cancel-long-press!)))
+                                               :on-touch-move (when (= kind :tag)
+                                                               (fn [_]
+                                                                 (cancel-long-press!)))
                                                }
                                   [:<>
                                    [:div {:style {:display        :flex
@@ -54,7 +91,14 @@
                                                   }
                                       (let [[tag-items other-items] @(subs/child-data-by-tag-id id)]
                                         [tag-submenu id tag-items other-items])])
-                                   ]])])
+                                    ]])
+   (when-let [{:keys [id position]} @context-menu*]
+     [:> Menu {:open             true
+               :on-close         close-context-menu!
+               :anchor-reference :anchorPosition
+               :anchor-position  position}
+       [:> MenuItem {:on-click #(open-tag! id)} "Open tag"]])
+    ])
 
 (defn tag-menu []
   (let [root-tag-items @subs/root-tag-data*]
